da3e6a06c98d91daf7a2dadeba80273456634b50
Ashutosh Kumar <ashutoshk.4300@gmail.com>
2021-03-30 11:02:25 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+port = 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.listen(port,function(){
+    console.log("To do list server is running.");
+})
+
+var item=[];
+
+var options = { weekday: 'long', 
+                year: 'numeric', 
+                month: 'long', 
+                day: 'numeric' };
+var today  = new Date();
+
+var day = today.toLocaleDateString("en-US", options);
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+
+    res.render("list",{whichday:day, itemList:item});
+
+
+})
+
+app.post('/', function(req, res){
+    item.push(req.body.nextThing);
+    console.log(item);
+    res.redirect("/");
+})
+
